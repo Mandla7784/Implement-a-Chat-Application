3111a3fcab36bad50aef3d980223fb7257deadeb
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-04-10 13:06:28 +0200

    Added client impelmenation

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -12,15 +12,24 @@ public class Client {
       int port =  Integer.parseInt(args[1]);
 
 
-
-
       try {
           System.out.println("Conneting to " + serverName + "listening on port :" + port);
           // creating a scoket for client object 
           Socket client  = new Socket(serverName , port);
+          System.out.println("Just connected to" + client.getRemoteSocketAddress());
+          OutputStream outputServer = client.getOutputStream();
+          DataOutput output = new DataOutputStream(outputServer);
+
+
+          output.writeUTF("Hello from" + client.getLocalAddress());
+
+
+
+          InputStream inFromServer = client.getInputStream();
+          DataInputStream in = new DataInputStream(inFromServer);
           
-
-
+          System.out.println("Server says " + in.readUTF());
+          client.close();
 
 
       } catch (IOException e) {

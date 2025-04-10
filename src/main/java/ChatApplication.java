3c75b9e17b7d52fa0b6463ed4ef32c835f598f61
mandyonacpt024 <mandyonacpt024@student.wethinkcode.co.za>
2025-04-10 12:24:58 +0200

    Added jernouling text file

--- src/main/java/ChatApplication.java
+++ src/main/java/ChatApplication.java
@@ -1,5 +1,18 @@
 package main.java;
 
+
+
+import java.io.*;
+
+/**
+ * Task: Implement a Chat Application
+
+    Develop a chat application using Java and
+    networking libraries like Socket or Java NIO.
+    This task will enhance their knowledge of
+    network programming, client-server
+    architecture, and message exchange.
+ */
 class ChatApplication{
     public static void main(String[] args){
         System.out.println("Hello user ");
